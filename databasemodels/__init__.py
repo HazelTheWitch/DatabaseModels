1231752322d70b1658ed456bfe19f6324f24d6f9
Hazel Rella <hrella@picarro.com>
2021-08-27 17:20:07 -0700

    Added TODO list

--- databasemodels/__init__.py
+++ databasemodels/__init__.py
@@ -1,3 +1,8 @@
 from .wrapper import *
 from . import datatypes as dtypes
 from .connection import *
+
+
+# TODO: Implement JSON type
+# TODO: Implement arrays
+# TODO: Implement composite types (maybe)
